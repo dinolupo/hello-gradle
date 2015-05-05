7fad8c0224647ac7d05481fe1576a0efafd721dc
Dino Lupo <dino.lupo@gmail.com>
2015-05-05 18:11:05 +0200

    readme update

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ Hello-Gradle
 
 Just an Hello World program with [`GRADLE`](http://gradle.org/) as a build system.
 
+## TIPS
+
+When you add a new library to the project, in this example Joda Time, you can do the following from the console:
+
+```sh
+# build to download libraries into your local repository
+gradle build
+# update eclipse project to reflect new changes about the Java build path
+gradle eclipse
+``` 
+and then Refresh you Eclipse Project.
--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,10 +1,16 @@
 package hello;
 
+import org.joda.time.LocalTime;
+
 public class HelloWorld {
 
 	public static void main (String[] args) {
+		
+		LocalTime currentTime = new LocalTime();
+	    System.out.printf("The current local time is: %s", currentTime);
+		
 		Greeter g = new Greeter();
-		System.out.printf("---------------\n%s\n---------------", g.sayHello());
+		System.out.printf("\n---------------\n%s\n---------------\n", g.sayHello());
 	}
 	
 }
